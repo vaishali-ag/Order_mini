6c674990e786d3c893a027ac62ac6cb9cbbf3b0f
Vaishali-ag <vaishali02071997@gmail.com>
2019-02-25 21:35:22 +0530

    Modify previous code add toast for order and reset

--- app/src/main/java/vaisahli/agrawal/order_mini/MainActivity.java
+++ app/src/main/java/vaisahli/agrawal/order_mini/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     Button Reset;
     Button Order;
     Button buttonMinus;
+    CheckBox whip,choco;
+    int  coffee=0;
+    int total=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,25 +29,74 @@ public class MainActivity extends AppCompatActivity {
         Quantity = findViewById(R.id.textView4);
         Reset = findViewById(R.id.button4);
         Order=findViewById(R.id.button3);
-        buttonPlus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int a = Integer.parseInt(Quantity.getText().toString().trim());
-                Quantity.setText(a+1);
-            }
-        });
+        whip = findViewById(R.id.checkBox);
+        choco = findViewById(R.id.checkBox2);
+
+        //int a = Integer.parseInt((Quantity.getText().toString()));
         buttonMinus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int b = Integer.parseInt((Quantity.getText().toString().trim()));
-                Quantity.setText(b-1);
+                if(coffee>0)
+                    coffee--;
+                Quantity.setText(String.valueOf(coffee));
+            }
+        });
+        buttonPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              coffee++;
+              Quantity.setText(String.valueOf(coffee));
 
             }
         });
         Order.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "fdfd", Toast.LENGTH_SHORT).show();
+                total=coffee*10;
+                if( whip.isChecked() && coffee>0 )
+                {
+                    total=total+5*coffee;
+                }
+                if(choco.isChecked()&&coffee>0)
+                {
+                    total=total+5*coffee;
+
+                }
+                if(coffee>0)
+                {
+                    if(whip.isChecked()&&choco.isChecked())
+                    {
+                        Toast.makeText(MainActivity.this, " You Have Selected " + String.valueOf(coffee) + " Coffee With Whipped Cream and  Chocolate. Your Bill Is: " + String.valueOf(total), Toast.LENGTH_SHORT).show();
+                    }
+                    else if(whip.isChecked())
+                    {
+                        Toast.makeText(MainActivity.this, " You Have Selected " + String.valueOf(coffee) + " Coffee With Whipped Cream. Your Bill Is: " + String.valueOf(total), Toast.LENGTH_SHORT).show();
+                    }
+                    else if(choco.isChecked())
+                    {
+                        Toast.makeText(MainActivity.this, " You Have Selected " + String.valueOf(coffee) + " Coffee With Chocolate. Your Bill Is: " + String.valueOf(total), Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        Toast.makeText(MainActivity.this, "You have selected "+ String.valueOf(coffee) +" with no Toppings.", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "No Cofee selscted.", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        Reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                total=0;
+                coffee=0;
+                Quantity.setText(String.valueOf(coffee));
+                whip.setChecked(false);
+                choco.setChecked(false);
             }
         });
 
