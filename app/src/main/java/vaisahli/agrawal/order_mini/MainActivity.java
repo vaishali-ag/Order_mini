65c5e45ebec75f664ed223881417680cd1a9b94a
Vaishali-ag <vaishali02071997@gmail.com>
2019-02-25 10:08:02 +0530

    layout set

--- app/src/main/java/vaisahli/agrawal/order_mini/MainActivity.java
+++ app/src/main/java/vaisahli/agrawal/order_mini/MainActivity.java
@@ -2,12 +2,25 @@ package vaisahli.agrawal.order_mini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText Quantity;
+    Button buttonPlus;
+    Button Reset;
+    Button Order;
+    Button buttonMinus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonMinus = findViewById(R.id.button2);
+        buttonPlus=findViewById(R.id.button);
+        //Quantity = findViewById(R.id.textView4);
+        Reset = findViewById(R.id.button4);
+        Order=findViewById(R.id.button3)
+
     }
 }
