a9e7ec94ea8d436b7814424adc594297845b5e82
Vaishali-ag <vaishali02071997@gmail.com>
2019-02-25 21:00:52 +0530

    code for + and - done

--- app/src/main/java/vaisahli/agrawal/order_mini/MainActivity.java
+++ app/src/main/java/vaisahli/agrawal/order_mini/MainActivity.java
@@ -2,12 +2,16 @@ package vaisahli.agrawal.order_mini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText Quantity;
+    //EditText Quantity;
+    TextView Quantity;
     Button buttonPlus;
     Button Reset;
     Button Order;
@@ -18,9 +22,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         buttonMinus = findViewById(R.id.button2);
         buttonPlus=findViewById(R.id.button);
-        //Quantity = findViewById(R.id.textView4);
+        Quantity = findViewById(R.id.textView4);
         Reset = findViewById(R.id.button4);
-        Order=findViewById(R.id.button3)
+        Order=findViewById(R.id.button3);
+        buttonPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt(Quantity.getText().toString().trim());
+                Quantity.setText(a+1);
+            }
+        });
+        buttonMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int b = Integer.parseInt((Quantity.getText().toString().trim()));
+                Quantity.setText(b-1);
+
+            }
+        });
+        Order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "fdfd", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
 }
